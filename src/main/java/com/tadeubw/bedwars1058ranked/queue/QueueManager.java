598a34fb507e3a6d9ebf57f81749da543ef049f3
zyypj <zyypjl@gmail.com>
2024-03-19 13:43:15 -0300

    BedWars Ranked

--- src/main/java/com/tadeubw/bedwars1058ranked/queue/QueueManager.java
+++ src/main/java/com/tadeubw/bedwars1058ranked/queue/QueueManager.java
@@ -4,11 +4,16 @@ import com.andrei1058.bedwars.api.BedWars;
 import com.tadeubw.bedwars1058ranked.Main;
 import com.tadeubw.bedwars1058ranked.elo.EloManager;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.scheduler.BukkitTask;
 
 import java.util.*;
@@ -26,6 +31,119 @@ public class QueueManager implements Listener {
         this.plugin = plugin;
         this.eloManager = eloManager;
         this.gameQueue = new HashMap<>();
+    }
+
+    @EventHandler
+    public void onClickEvent(InventoryClickEvent e) {
+        Player player = (Player) e.getWhoClicked();
+        if (e.getView().getTitle().equalsIgnoreCase("§7Entrar em uma Fila")) {
+            if (e.getCurrentItem().getType() == Material.BED && e.getCurrentItem().getAmount() == 1) {
+                UUID playerUUID = player.getUniqueId();
+                if (bedwarsAPI.getPartyUtil().hasParty(player)) {
+                    player.sendMessage("§cVocê não pode entrar em uma fila ranqueada em party!");
+                    player.closeInventory();
+                    player.playSound(player.getLocation(), Sound.NOTE_BASS, 1, 1);
+                    return;
+                }
+                if (bedwarsAPI.getStatsUtil().getPlayerWins(playerUUID) >= 100 || player.hasPermission("bw.vip")) {
+                    if (bedwarsAPI.getArenaUtil().isPlaying(player)) {
+                        player.sendMessage("§cVocê já está jogando!");
+                        player.closeInventory();
+                        player.playSound(player.getLocation(), Sound.NOTE_BASS, 1, 1);
+                        return;
+                    }
+                    joinQueue(player, "Ranked1v1");
+                    player.closeInventory();
+                    return;
+                }
+                player.sendMessage("§cPara entrar numa fila ranqueada você precisa de\n§c§l+100 WINS §cou comprar §e§lVIP§c.");
+                player.closeInventory();
+                player.playSound(player.getLocation(), Sound.NOTE_BASS, 1, 1);
+                return;
+            }
+            if (e.getCurrentItem().getType() == Material.BED && e.getCurrentItem().getAmount() == 4) {
+                if (!(bedwarsAPI.getPartyUtil().hasParty(player))) {
+                    player.sendMessage("§cVocê precisa estar em uma party para entrar nessa fila!");
+                    player.closeInventory();
+                    player.playSound(player.getLocation(), Sound.NOTE_BASS, 1, 1);
+                    return;
+                }
+                if (bedwarsAPI.getPartyUtil().hasParty(player)) {
+                    if (!(bedwarsAPI.getPartyUtil().isOwner(player))) {
+                        player.sendMessage("§cApenas o dono da party pode entrar em uma fila ranqueada!");
+                        player.closeInventory();
+                        player.playSound(player.getLocation(), Sound.NOTE_BASS, 1, 1);
+                        return;
+                    }
+                    List<Player> members = bedwarsAPI.getPartyUtil().getMembers(player);
+                    if (members.size() != 4) {
+                        player.sendMessage("§cPara jogar 4v4, você precisa de exatamente 4 jogadores em party!");
+                        player.closeInventory();
+                        player.playSound(player.getLocation(), Sound.NOTE_BASS, 1, 1);
+                        return;
+                    }
+                    UUID playerUUID = player.getUniqueId();
+                    if (bedwarsAPI.getStatsUtil().getPlayerWins(playerUUID) >= 100 || player.hasPermission("bw.vip")) {
+                        if (bedwarsAPI.getArenaUtil().isPlaying(player)) {
+                            player.sendMessage("§cVocê já está jogando!");
+                            player.closeInventory();
+                            player.playSound(player.getLocation(), Sound.NOTE_BASS, 1, 1);
+                            return;
+                        }
+                        openJoinMenu(player);
+                        for (Player member : members) {
+                            if (member != player) {
+                                member.sendMessage("");
+                                member.sendMessage("§7A party entrou numa fila de partida 4v4 ranqueada.");
+                                member.sendMessage("");
+                                player.playSound(player.getLocation(), Sound.LEVEL_UP, 1, 1);
+                                player.closeInventory();
+                                return;
+                            }
+                        }
+                    }
+                    player.sendMessage("§cPara entrar numa fila ranqueada você precisa de\n§c§l+100 WINS §cou comprar §e§lVIP§c.");
+                    player.closeInventory();
+                    player.playSound(player.getLocation(), Sound.NOTE_BASS, 1, 1);
+                    return;
+                }
+                joinQueue(player, "Ranked4v4");
+                player.closeInventory();
+                return;
+            }
+            e.setCancelled(true);
+        }
+    }
+
+    public void openJoinMenu(Player player) {
+        Inventory testMenu = Bukkit.createInventory(player, 27, "§7Entrar em uma Fila");
+
+        ItemStack Ranked1Item = new ItemStack(Material.BED);
+        ItemMeta Ranked1Meta = Ranked1Item.getItemMeta();
+        Ranked1Meta.setDisplayName("§aRanked 1v1");
+        List<String> lore = new ArrayList<>();
+        lore.add("§7Entrar na fila para");
+        lore.add("§7§lRanked1v1");
+        lore.add("");
+        lore.add("§eClique para entrar na fila");
+        Ranked1Meta.setLore(lore);
+        Ranked1Item.setItemMeta(Ranked1Meta);
+        testMenu.setItem(12, Ranked1Item);
+
+        ItemStack Ranked4Item = new ItemStack(Material.BED);
+        ItemMeta Ranked4Meta = Ranked4Item.getItemMeta();
+        Ranked4Meta.setDisplayName("§aRanked 4v4");
+        List<String> lore4 = new ArrayList<>();
+        lore4.add("§7Entrar na fila para");
+        lore4.add("§7§lRanked4v4");
+        lore4.add("");
+        lore4.add("§eClique para entrar na fila");
+        Ranked4Meta.setLore(lore4);
+        Ranked4Item.setItemMeta(Ranked4Meta);
+        Ranked4Item.setAmount(4);
+        testMenu.setItem(14, Ranked4Item);
+
+        player.openInventory(testMenu);
     }
 
     public void joinQueue(Player player, String gameType) {
